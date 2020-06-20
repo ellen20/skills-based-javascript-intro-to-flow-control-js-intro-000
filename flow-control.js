9f7a0d2e3da3a616fe4770243632e84ea2199b1d
jingling jin <jljin0524@gmail.com>
2020-06-20 21:35:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,6 +29,7 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
+  switch (age){
   case 13:
     return "You are a teenager"
   case 14:
@@ -45,4 +46,5 @@ function switchAge(age) {
     return "You are a teenager"
   default:
     return "You have an age"
+  }
 }
